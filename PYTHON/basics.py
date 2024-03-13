34111c48956ae2dcfdb9207de11e2c8dd6c8efa5
Aryan Mungekar <aryan.mungekar@gmail.com>
2024-03-13 19:13:32 +0530

    new function

--- PYTHON/basics.py
+++ PYTHON/basics.py
@@ -4,6 +4,9 @@ print("Hello World")
 name = "hello" #string
 age = 21 #integer
 time = 21.2 #float valve
+old = False #boolean is true or false
 
+#type function
+print(type(name)) #tells the data type
  
 
---
+++ PYTHON/tupple.py
@@ -0,0 +1,7 @@
+tuple1 = [1, 2, 3]
+list = ["hello","hey"]
+print(tuple1[0])
+print(list[1])
+
+#list is non mutable whereas tuple is mutable
+#ie. we cannot change the values of tuple once declared
