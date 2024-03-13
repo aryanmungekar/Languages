d950363dbf5443d7e977c709d8bee23c9321aa91
Aryan Mungekar <aryan.mungekar@gmail.com>
2024-03-13 20:17:43 +0530

    typecasting

---
+++ PYTHON/typecasting.py
@@ -0,0 +1,20 @@
+#here we can perform basic operation on string type of variables just by declaring the datatype before the operation
+a = "1"
+b = "3"
+c = 3
+
+print(int(a) + int(b) + c)
+
+
+#here we coinvert float to int before the operation therefore the value of a = 2 now.
+a = 2.2
+b = 4
+
+print(int(a)+b)
+
+#implicite typecasting- here the compiler converts int to folat to give more accurate answer
+
+a = 12.3
+b = 4
+
+print(a+b)
