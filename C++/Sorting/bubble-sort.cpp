21be316ca8d85305c24e458d1529bdd0fd941707
Aryan Mungekar <aryan.mungekar05@gmail.com>
2024-09-19 23:46:14 +0530

    bubble sort

---
+++ C++/Sorting/bubble-sort.cpp
@@ -0,0 +1,37 @@
+// bubble sort
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    
+    int arr[]={7,6,5,4,3,2,1};
+    
+    int n= sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i=n-2; i>=0; i--)
+    {
+        bool swapped=0;
+        for(int j=0; j<=i; j++ )
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+                swapped=1;
+            }
+        }
+        if(swapped==0)
+        break;
+
+    }
+    
+    
+    
+    // printing array
+    for(int k=0; k<n; k++)
+    {
+        cout<<arr[k]<<" ";
+    }
+
+    return 0;
+}
